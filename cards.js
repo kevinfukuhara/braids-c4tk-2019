2e97d0a22c11dbc5fa8f8b03fdca7370259c00fb
cltang <cltang01@gmail.com>
2019-05-04 15:20:17 -0700

    delay modal content render until braid details request comes back

--- cards.js
+++ cards.js
@@ -29,33 +29,36 @@ var braidArr = [braidexample1, braidexample2, braidexample3]
 
 // TODO: remove hard-coded stuff above. Do an actual PactList() API call instead.
 
-const renderBraidCard = (braid, i) => {
-  // TODO: use a templating library like Closure or Jinga
+// TODO: use a templating library like Closure or Jinga
+const getCardHtml = (braid, i) => {
   const dataAttribute = "data-braid-id='" 
-    // encode the Braid ID into the DOM to be used for looking up details (current status of all participants, history/streaks).
+    // encode the Braid ID into the DOM to be used for looking up details
     + braid.braidId + "'";
 
   const modalTriggerHtml = "<!-- Modal Trigger --><a class='waves-effect waves-light btn modal-trigger' href='#modal" 
     + i + "'" + dataAttribute +">See More</a>";
 
+  return "<div class='col s12 m4'><div class='card'><div class='card-image'><img src='" 
+  + braid.image +"'><span class='card-title'>" 
+  + braid.name + "</span></div><div class='card-content'><p>" 
+  + braid.desc + "</p></div><div class='card-action'>" 
+  + modalTriggerHtml + "</div></div";
+};
+
+/** @param {FullBraid} */
+const getBraidDetailsModalHtml = (braid) => {
+  // TODO: flesh this out with actual full braid details (current status of all participants, history/streaks).
   const modalContentHtml = "<!-- Modal Structure --><div id='modal" 
     + i + "' class='modal modal-fixed-footer'><div class='modal-content'><h4>" 
-    + braid.name + "</h4><p>" + braid.desc + "</p></div><div class='modal-footer'><a href='#!' class='modal-close waves-effect waves-green btn-flat'>Close</a></div></div></div>";
-
-  const renderCardHtml = (modalTriggerHtml, modalContentHtml) => {
-    return "<div class='col s12 m4'><div class='card'><div class='card-image'><img src='" 
-    + braid.image +"'><span class='card-title'>" 
-    + braid.name + "</span></div><div class='card-content'><p>" 
-    + braid.desc + "</p></div><div class='card-action'>" 
-    + modalTriggerHtml + modalContentHtml + "</div></div";
-  };
-
-  $("#braid").append(renderCardHtml(modalTriggerHtml, modalContentHtml));
+    + braid.name + "</h4><p>" + braid.desc + "</p></div><div class='modal-footer'>"
+    + "<a href='#!' class='modal-close waves-effect waves-green btn-flat'>Close</a></div></div></div>";
+  return modalContentHtml;
 }
 
 if (braidArr != -1) {
   for (i in braidArr) {
-    renderBraidCard(braidArr[i], i);
+    // Defer adding modal content until full details are loaded.
+    $("#braid").append(getCardHtml(braidArr[i], i));
   }
 }
 else {
@@ -69,7 +72,8 @@ $('.modal-trigger').click((e) => {
   // Get braid details and render.
   requestService.getBraid(braidId).then(braid => {
     print(' got braid by id: ' + braid);
-    // TODO: render braid details inside modal.
+    // TODO: render braid details inside modal now that it's fetched.
+    $('card-action').append(getBraidDetailsModalHtml(braid));
   });
 });
 
