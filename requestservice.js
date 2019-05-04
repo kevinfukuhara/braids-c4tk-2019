8438e9eba6037047c3997c28c53134792d85d34e
cltang <cltang01@gmail.com>
2019-05-04 11:04:07 -0700

    2

--- requestservice.js
+++ requestservice.js
@@ -1,3 +1,5 @@
+console.log('loaded request service');
+
 // overkill for now?
 class CreateRequest {
 	constructor(braid) {
@@ -5,27 +7,43 @@ class CreateRequest {
 	}
 }
 
-// TODO: class
+// TODO: class for type safety
 const braid = {
   name: 'Devotional',
   desc: 'Read the Word every morning',
-  freq: 'daily', // TODO: enum
+  freq: 'daily', // TODO: string enum
+  creatorUsername: 'cltang01', // assume unique
+  createdAt: 1556992380000, // millis since epoch
 };
 
 class TransportService {
-	constructor() {
-		this.url = 'https://' + 'localhost:9000';
+	constructor(host) {
+		this.url = host;
 		// this.authUser = 
 	}
 
-	createBraid() {
+	getUsers() {
+		const path = '/users';
+		const data = {};
+		return this.send_(path, data);
+	}
+
+	getGroups() {
+		const path = '/groups';
+		const data = {};
+		return this.send_(path, data);
+	}
+
+	createBraid(braid) {
 		const path = '/create';
+		const data = braid; //TODO: transform into expected API format		
 // TODO: url args?
 		return this.send_(path, data);
 	}
 
-	getBraid() {
-		const path = '/get';
+	getBraid(braidId) {
+		const path = '/get/' + braidId;
+		const data = {};
 		return this.send_(path, data);
 	}
 
@@ -33,10 +51,12 @@ class TransportService {
 	/** @return {!Promise<!Braid>} */
 	getAllBraids() {
 		const path = '/getAll';
+		const data = {};
 		return this.send_(path, data);
 	}
 
 	send_(path, data) {
+		// FIXME: configure origin
 		const settings = {
 			// beforeSend:
 			data,
@@ -57,9 +77,10 @@ class TransportService {
 		    },
 		};
 
-		// TODO: send xhr to url+path
-		jQuery.ajax(this.url+path, settings);
-		// return Promise.resolve(result);
+		return jQuery.ajax(this.url+path, settings).then(response => {
+			// TODO: process response into Braid object
+			return response;
+		});
 	}
 
 }
@@ -68,10 +89,13 @@ class TransportService {
 // Test script
 const req = new CreateRequest(braid);
 
+// const host = 'https://localhost:9000';
+// const host = 'https://httpbin.org/get'; // tester
+const host = 'https://braids-app.azurewebsites.net/'; // actual API
 const transportService = new TransportService();
 transportService.createBraid(req); //.then(response => {});
-transportService.getAllBraids(req); //.then(response => {});
-transportService.getBraid(req); //.then(response => {});
+// transportService.getAllBraids(req); //.then(response => {});
+// transportService.getBraid(req); //.then(response => {});
 //
 
 //
